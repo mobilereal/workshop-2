932e37d879f4af7ab2bc8135d6259448358bd0b5
mobilereal <suchanat.l@ku.th>
2021-02-06 23:42:04 +0700

    add edit for

--- if_else/ex_2.py
+++ if_else/ex_2.py
@@ -1,10 +1,20 @@
-opre = 2
-while opre <= 12:
-    print("[", opre, "]")
-    num = 1
-    while num <= 12:
-        print(opre, "*", num, ":", opre * num, " ")
-        num += 1
+oper1 = 2
+while oper1 <= 12:
+    print("[", oper1, "]")
+    num1 = 1
+    while num1 <= 12:
+        print(oper1, "*", num1, ":", oper1 * num1, " ")
+        num1 += 1
     print("")
-    opre += 1
-    print("---------------------")
+    oper1 += 1
+    print("---------------")
+
+#######################################################
+
+table = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for table in table:
+    print("[", table, "]")
+    for num in number:
+        print(table, " * ", num, " : ", table * num)
+    print("------------")

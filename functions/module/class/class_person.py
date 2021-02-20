1f08a37ba10a94b75262eab264deeab0489c2b17
mobilereal <suchanat.l@ku.th>
2021-02-20 11:15:25 +0700

    add new class

--- functions/module/class/class_person.py
+++ functions/module/class/class_person.py
@@ -1,6 +1,6 @@
-class  Person:
-   def  __init__(self,name,age,address):
-        self.name = name 
+class Person:
+    def __init__(self, name, age, address):
+        self.name = name
         self.age = age
         self.address = address
 
@@ -10,8 +10,8 @@ class  Person:
     def get_age(self):
         return self.age
 
-    def  get_address(self):
+    def get_address(self):
         return self.address
 
     def get_info(self):
-        return {"name": self.name,"age" : self.age}
+        return {"name": self.name, "age": self.age}
